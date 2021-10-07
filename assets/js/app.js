55fea494c74aa81580af70acd1e7e7a0f5f71a07
Mohaned Adel <mohaned.adel1996@gmail.com>
2021-10-07 17:53:02 +0200

    modified: navLinks icon in mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,24 @@
+// Nav hamburgerburger selections
+
+const burger = document.querySelector('#burger-menu');
+const nav = document.querySelector('nav');
+const ul = document.querySelector('nav ul');
+
+burger.addEventListener('click', () => {
+    ul.classList.toggle('show')
+})
+
+// close navbar when a link is clicked
+//select nav links
+
+const navLink = document.querySelectorAll('.nav-link');
+// console.log(navLink); 
+navLink.forEach((link) => {
+    link.addEventListener('click', () => {
+        ul.classList.remove('show');
+    })
+})
+
 // scroll to top 
 const scrollUp = document.querySelector('#scroll-up');
 
